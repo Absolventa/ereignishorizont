78394a39eda1fa326f0589f128af1c341ec4251a
Susanne Dewein <susanne.dewein@gmail.com>
2013-07-04 11:58:12 +0200

    Make users#index the root page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ EventGirl::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'users#index'
   get '/registered_events' => 'pages#index'
 
   # Example of regular route:
