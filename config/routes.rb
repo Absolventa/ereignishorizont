df2f31d4f4cc91a106c59d9fc32d485f58d5dda7
Susanne Dewein <susanne.dewein@gmail.com>
2013-07-04 11:40:23 +0200

    Map /registered_events to pages#index

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ EventGirl::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  get '/registered_events' => 'pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
