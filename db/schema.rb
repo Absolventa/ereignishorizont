b0f2e9351d14610edabba5364019c9da40bf1e1b
Tam Eastley <tam.eastley@gmail.com>
2013-07-05 11:06:41 +0200

    Migrate the incoming_events controller database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130703083401) do
+ActiveRecord::Schema.define(version: 20130705083557) do
+
+  create_table "incoming_events", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
