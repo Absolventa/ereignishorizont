1fea451ff82000bdbbb1cfaa8adbe100a9aefd31
Susanne Dewein <susanne.dewein@gmail.com>
2013-07-10 11:41:38 +0200

    Change "set_expected_event" to "set_alarm_trigger"
    We changed the naming in line 2 (for some reason the changes are not shown in this file), which was a left-over from scaffolding (copied from the Expected Events). 
    In line 14 we deleted the plural in the instance variable.

--- app/controllers/alarm_triggers_controller.rb
+++ app/controllers/alarm_triggers_controller.rb
@@ -1,5 +1,66 @@
 class AlarmTriggersController < ApplicationController
+  before_action :set_alarm_trigger, only: [:show, :edit, :update, :destroy]
+  #buttons to show, edit, update and destroy only show up when 
+  #someone has entered an expected event. 
+
   def index
+  	@alarm_triggers = AlarmTrigger.all
   end
+  
+  def show
+  end
+
+  def new
+  	@alarm_trigger = AlarmTrigger.new
+  end
+
+  def edit
+  end
+
+  def create
+  	@alarm_trigger = AlarmTrigger.new(alarm_trigger_params)
+
+  	respond_to do |format|
+  		if @alarm_trigger.save
+  			format.html { redirect_to @alarm_trigger, notice: 'Alarm Trigger was successfully created'}
+  			format.json { render action: 'show', status: :created, location: @alarm_trigger }
+  		else
+  			format.html { render action: 'new' }
+  			format.json { render json: @alarm_trigger.errors, status: :unprocessable_entry }
+  		end
+  	end
+  end
+
+
+  def update
+  	respond_to do |format|
+  		if @alarm_trigger.update(alarm_trigger_params)
+  			format.html { redirect_to @alarm_trigger, notice: 'Alarm Trigger was successfully updated' }
+  			format.json { head :no_content }
+  		else
+  			format.html { render action: 'edit' }
+  			format.json { render json: @alarm_trigger.errors, status: :unprocessable_entry }
+  		end
+  	end
+  end
+
+ 
+  def destroy
+  	@alarm_trigger.destroy
+  	respond_to do |format|
+  		format.html { redirect_to alarm_trigger_url }
+  		format.json { head :no_content }
+  	end
+  end
+
+	private
+   		
+    	def set_alarm_trigger
+      		@alarm_trigger = AlarmTrigger.find(params[:id])
+    	end
+
+    	def alarm_trigger_params
+      		params.require(:alarm_trigger).permit(:event)
+    	end
 
 end
