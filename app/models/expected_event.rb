4f4e163c06c4f077e46ba5bcd769cd87bf905d04
Tam Eastley <tam.eastley@gmail.com>
2013-07-16 15:37:39 +0200

    Redo validates presence of command

--- app/models/expected_event.rb
+++ app/models/expected_event.rb
@@ -1,6 +1,5 @@
 class ExpectedEvent < ActiveRecord::Base
-	validates_presence_of :title
-	validates :title, format: { with: /\A[a-z\s]+\Z/i }
+	validates :title, presence: true, format: { with: /\A[a-z\s]+\Z/i }
 
 	has_many :alarms
 	has_many :incoming_events
