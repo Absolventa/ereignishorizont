be9a7e3500b2c930e5ce329850036eee082531c4
Tam Eastley <tam.eastley@gmail.com>
2013-07-16 15:36:32 +0200

    Validate format of title

--- app/models/expected_event.rb
+++ app/models/expected_event.rb
@@ -1,5 +1,7 @@
 class ExpectedEvent < ActiveRecord::Base
 	validates_presence_of :title
+	validates :title, format: { with: /\A[a-z\s]+\Z/i }
+
 	has_many :alarms
 	has_many :incoming_events
 end
--- spec/models/expected_event_spec.rb
+++ spec/models/expected_event_spec.rb
@@ -4,4 +4,12 @@ describe ExpectedEvent do
 	 it { should have_many :alarms }
 	 it { should have_many :incoming_events }
 	 it { should validate_presence_of :title }
+
+	 context 'validating title' do
+	 	it 'complains about umlauts' do
+	 		expected_event = ExpectedEvent.new(title: 'böse')
+	 		expected_event.valid?
+	 		expected_event.should have(1).error_on(:title)
+	 	end
+	 end
 end
