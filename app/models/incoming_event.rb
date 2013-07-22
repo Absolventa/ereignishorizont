225c4803580f02284497511eb9e3982d9c0b4a0d
Tam Eastley <tam.eastley@gmail.com>
2013-07-22 15:13:12 +0200

    Create rspec test for incoming event

--- app/models/incoming_event.rb
+++ app/models/incoming_event.rb
@@ -1,4 +1,13 @@
 class IncomingEvent < ActiveRecord::Base
-	validates_presence_of :title
+	validates :title, presence: true, format: { with: /\A[a-z0-9\s]+\Z/i }
+
 	belongs_to :expected_event
+
+	before_save :delete_white_spaces_from_title
+
+  private
+    def delete_white_spaces_from_title
+      self.title = self.title.strip
+    end
 end
+
---
+++ spec/models/incoming_event_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe IncomingEvent do
+	 it { should validate_presence_of :title }
+	 it { should belong_to :expected_event }
+
+	 context 'validating title' do
+	 	it 'complains about illegal characters' do
+	 		incoming_event = IncomingEvent.new(title: 'bß€se')
+	 		incoming_event.valid?
+	 		incoming_event.should have(1).error_on(:title)
+	 	end
+
+	 	it 'removes trailing white spaces before save' do
+	 		incoming_event = IncomingEvent.new(title: ' bose    ')
+	 		incoming_event.save
+	 		incoming_event.title.should == 'bose'
+	 	end
+	 end
+end
