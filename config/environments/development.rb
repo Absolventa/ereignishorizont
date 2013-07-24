b8b6d4b7c56721e26a9c7a33e3eb8fba2ef62f7f
Tam Eastley <tam.eastley@gmail.com>
2013-07-24 16:27:27 +0200

    Make config changes to try to send email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ EventGirl::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -26,4 +26,14 @@ EventGirl::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i -t'
+  # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-replay@example.com'}
 end
