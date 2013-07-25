03345506cfb310394b3fc793b8a9eec5f9647ac2
Tam Eastley <tam.eastley@gmail.com>
2013-07-25 17:57:00 +0200

    Add message to email template

--- app/mailers/alarm_mailer.rb
+++ app/mailers/alarm_mailer.rb
@@ -4,6 +4,7 @@ class AlarmMailer < ActionMailer::Base
   def event_expectation_matched(alarm)
   	@alarm = alarm
   	@expected_event = alarm.expected_event.title
+  	@message = alarm.message
   	mail(:to => alarm.recipient_email, :subject => "Don't be alarmed!")
   	#mail call should always be at the end
   end
