dc5957518562467d97a6ffc430c46695bf423cb6
Tam Eastley <tam.eastley@gmail.com>
2013-07-25 17:57:29 +0200

    Attempt at validates_exclusion_of for the logger in alarms
    THIS DOESN'T WORK!

--- app/models/alarm.rb
+++ app/models/alarm.rb
@@ -3,10 +3,16 @@ class Alarm < ActiveRecord::Base
   belongs_to :expected_event
 
   validates_presence_of :recipient_email, :if => :enters_email?
+  #validates_exclusion_of :recipient_email, :in => :enters_logger,
+                          #:message => "Why are you putting in your email? This is for logs!"
 
   def enters_email?
   	action == 'Email'
   end
+
+  #def enters_logger
+    #action == 'Logger'
+  #end
 
   def run
     ## Do stuff based on my action
