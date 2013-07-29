c30be3d0588b415823268ae21b67ecf7aa7c1d08
Tam Eastley <tam.eastley@gmail.com>
2013-07-29 12:52:07 +0200

    Add validates_inclusion_of for action
    We're not entirely sure if this is right or if this is what Carsten meant. We think that we have to somehow go into the browser and see if we can change the drop down menu (for email and logger) - and we think that in theory this validation works as a security feature which will stop ppl from externally messing around with our app. However, we're not sure if this works/how to do this/if this is the case. We don#t get an error message though, which is good. TBC when Carsten is zurück.

--- app/models/alarm.rb
+++ app/models/alarm.rb
@@ -5,7 +5,11 @@ class Alarm < ActiveRecord::Base
   validates :recipient_email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create },
                               presence: true,
                               if: :enters_email?
-  
+
+ validates_inclusion_of :action, in: %w( Email Logger)
+    #we're not entirely sure if this is working/is correct. Do we have to 
+    #do something in the browser/try to change it in the browser to see if
+    #it is secure against external hacks? HELP!
 
   def enters_email?
     action == 'Email'
