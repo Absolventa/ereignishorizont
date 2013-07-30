b10d9962dea02d4d107f2c63326837dfc4ad299c
Susanne Dewein <susanne.dewein@gmail.com>
2013-07-30 17:54:50 +0200

    Create User model
    We added has_secure_password which adds some simple authentication support to the model.
    We also added several validations.

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  validates_uniqueness_of :email
+
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create },
+                              presence: true
+end
