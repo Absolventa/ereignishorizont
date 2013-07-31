886486fb6e1962a0dfc5bc6034269d8a783daf92
Tam Eastley <tam.eastley@gmail.com>
2013-07-31 17:14:25 +0200

    Short explanation of the ||= boolean expression

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,11 @@ class ApplicationController < ActionController::Base
   private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    # short form for: 
+    # @current_user = @current_user || User.find(session[:user_id]) if session[:user_id]
+    # if @current_user = @current_user then the expression stops and is true
+    # if @current user does not equal current user, than the second part of the
+    # expression is evaluated. (See Tobi's email) 
   end
   helper_method :current_user
 end
