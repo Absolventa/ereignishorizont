57c61403ae8d28d1b653310782ff5d9fbc6b1d5c
Susanne Dewein <susanne.dewein@gmail.com>
2013-07-31 16:06:21 +0200

    Create create action in Sessions Controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "Logged in!"
+    else
+      flash.now.alert = "Email or password is invalid."
+    end
+  end
+end
+
+
