5f952d6d37328581df55ecf1664e43caf1c81249
Tam Eastley <tam.eastley@gmail.com>
2013-07-31 17:14:36 +0200

    Remove unnecessary code from users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,7 @@ respond_to :html
 
   def create
     @user = User.new(user_params)
-
-    # respond_to do |format|
-    #   if @user.save
-    #     format.html { redirect_to root_url, notice: 'Zank u for signing up!' }
-    #   else
-    #     format.html { render action: "new" }
-    #   end
-    # end
+      
       if @user.save
         redirect_to root_url
         flash[:notice] = "Zank u for signing up!"
@@ -23,7 +16,7 @@ respond_to :html
         render action: "new"
         flash[:error] = "Your sign in sucked"
       end
-
+      
   end
 
 
