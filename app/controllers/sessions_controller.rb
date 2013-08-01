388320777cab9ac9071f67621d43ff28683c5198
Susanne Dewein <susanne.dewein@gmail.com>
2013-08-01 11:49:05 +0200

    Add destroy action to sessions_controller
    This is in done in order to be able to log out.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,11 @@ class SessionsController < ApplicationController
       flash.now.alert = "Email or password is invalid."
     end
   end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "Logged out!"
+  end
 end
 
 
