19374dca782d44618e8bfd50f806aa0eee725cdc
Susanne Dewein <susanne.dewein@gmail.com>
2013-08-02 11:14:45 +0200

    Change redirect paths for login and add flash msg

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,10 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, notice: "Logged in!"
+      redirect_to incoming_events_path, notice: "Logged in!"
     else
-      flash.now.alert = "Email or password is invalid."
+      redirect_to root_url
+      flash[:error] = "Email or password is invalid."
     end
   end
 
