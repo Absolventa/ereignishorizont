067966980c5bc65a26dd0a0cf7463cb8df0d7d22
Tam Eastley <tam.eastley@gmail.com>
2013-08-05 15:14:02 +0200

    Add validation for password length in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates_uniqueness_of :email
+  validates :password, length: 8..20
 
-    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create },
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create },
                               presence: true
 end
