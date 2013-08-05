a59ecfd3e79e3a913ad0c7d419b598b18e8895b0
Tam Eastley <tam.eastley@gmail.com>
2013-08-05 17:51:08 +0200

    Add additional test for alarm model

--- spec/models/alarm_spec.rb
+++ spec/models/alarm_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 
 describe Alarm do
+
   it { should belong_to :expected_event }
   it { should validate_presence_of :expected_event }
+	it { should allow_value("a@b.com").for(:recipient_email) }
+	#it { should_not allow_value("blah").for(:recipient_email) } <= this doesn't work!
 end
+
+# how does rspec work for boolean values?
