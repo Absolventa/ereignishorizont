58987043dbbcb9d8cae28ab570e28dd585429a6c
Susanne Dewein <susanne.dewein@gmail.com>
2013-08-06 14:21:50 +0200

    Create password_resets_controller
    Add edit, create and update to it. (And private methods, although they are not yet working..)

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,32 @@
+class PasswordResetsController < ApplicationController
+  def new
+  end
+
+  def edit
+    @user = User.find_by_password_reset_token!(params[:id])
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+    user.send_password_reset if user
+    redirect_to root_url, :notice => "Email sent with password reset instructions."
+  end
+
+  def update
+    @user = User.find_by_password_reset_token!(params[:id])
+    if @user.password_reset_sent_at < 2.hours.ago
+      redirect_to new_password_reset_path, :alert => "Password &crarr; reset has expired."
+    elsif @user.update_attributes(@password)#(params[:user])
+      redirect_to root_url, :notice => "Password has been reset."
+    else
+      render :edit
+    end
+  end
+
+private
+
+  def password_reset_params
+    params.require(:password_reset).permit([:password])
+  end
+
+end
