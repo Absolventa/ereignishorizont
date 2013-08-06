d44e1248b62c26e188e266ffdc774880baede770
Susanne Dewein <susanne.dewein@gmail.com>
2013-08-06 17:50:50 +0200

    Fix Reset Password
    Due to some changes in Rails 4 in line 20 we had to add "permit!" to make it work. Before the password couldn't be updated when coming from the link that was mailed to the user. (It always said the password is too short)

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,5 @@
 class PasswordResetsController < ApplicationController
+
   def new
   end
 
@@ -16,17 +17,11 @@ class PasswordResetsController < ApplicationController
     @user = User.find_by_password_reset_token!(params[:id])
     if @user.password_reset_sent_at < 2.hours.ago
       redirect_to new_password_reset_path, :alert => "Password &crarr; reset has expired."
-    elsif @user.update_attributes(@password)#(params[:user])
+    elsif @user.update_attributes(params.permit![:user])
       redirect_to root_url, :notice => "Password has been reset."
     else
       render :edit
     end
-  end
-
-private
-
-  def password_reset_params
-    params.require(:password_reset).permit([:password])
   end
 
 end
