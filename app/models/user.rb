d0ea92c2a01975f6785c7460202ecf14336a1df5
Susanne Dewein <susanne.dewein@gmail.com>
2013-08-06 14:32:11 +0200

    Add send_password_reset method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates_uniqueness_of :email
-  validates :password, length: 8..20
+  validates :password, length: 5..20
 	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create },
                               presence: true
 
@@ -11,5 +11,13 @@ class User < ActiveRecord::Base
  		begin
 			self[column] = SecureRandom.urlsafe_base64
 		end while User.exists?(column => self[column])
-	end                        
+	end 
+
+  def send_password_reset
+    generate_token(:password_reset_token)
+    self.password_reset_sent_at = Time.zone.now
+    save!(:validate => false)
+    UserMailer.password_reset(self).deliver
+  end
+
 end
