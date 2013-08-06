d7099fce4aaa290e8722f8d0dab48bd5c6d27be6
Tam Eastley <tam.eastley@gmail.com>
2013-08-06 11:32:10 +0200

    Create auth token column in users db table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130730142343) do
+ActiveRecord::Schema.define(version: 20130805155521) do
 
   create_table "alarms", force: true do |t|
     t.string  "title"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20130730142343) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "auth_token"
   end
 
 end
