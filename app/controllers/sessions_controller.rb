193ac3badff9962b9edb0dfc8778ff2cab57f446
Susanne Dewein <susanne.dewein@gmail.com>
2013-08-08 17:02:33 +0200

    Add redirect from root page if logged in
    We added a before_action and a private method that is called in the the before_action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,9 @@
 class SessionsController < ApplicationController
 
-  skip_before_action :authorize
-#how do we add an email validation to the session? There is no model!
+skip_before_action :authorize 
+before_action :redirect_if_logged_in, :only => [:new, :create]  
 
-  def new
-  end
+#how do we add an email validation to the session? There is no model!
 
   def create
     user = User.find_by_email(params[:email])
@@ -24,6 +23,12 @@ class SessionsController < ApplicationController
   def destroy
     cookies.delete(:auth_token)
     redirect_to root_url, notice: "Logged out!"
+  end
+
+private
+
+  def redirect_if_logged_in
+    redirect_to(incoming_events_path) if current_user
   end
 end
 
