5c12d1d9c8735305f930bacffbf8008173673619
Carsten Zimmermann <carp@hacksocke.de>
2013-08-09 12:03:02 +0200

    Add specs for users_controller#index

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe UsersController do
+  render_views
+
+  before do
+    @user = FactoryGirl.create(:user, admin: false)
+    controller.stub(:current_user).and_return(@user)
+  end
+
+  describe 'GET index' do
+    context 'as admin' do
+      it 'renders a list of all users' do
+        User.any_instance.stub(:admin?).and_return(true)
+        get :index
+        expect(response).to render_template 'index'
+      end
+    end
+
+    context 'as normal user' do
+      it 'redirects to incoming_events#index' do
+        get :index
+        expect(response).to redirect_to incoming_events_path
+      end
+    end
+  end
 
 end
