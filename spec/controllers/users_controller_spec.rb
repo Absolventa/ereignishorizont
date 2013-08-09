b4332247bf51d1e8562d28bc3c57b0383648d8ed
Carsten Zimmermann <carp@hacksocke.de>
2013-08-09 12:43:09 +0200

    Add specs for users#edit

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -44,4 +44,24 @@ describe UsersController do
     end
   end
 
+  describe 'GET edit' do
+    context 'as admin' do
+      it "renders the 'edit' template" do
+        User.any_instance.stub(:admin?).and_return(true)
+        other_user = FactoryGirl.create(:user)
+        get :edit, id: other_user.id
+        expect(response).to render_template 'edit'
+        expect(assigns(:user)).to eql other_user
+      end
+    end
+
+    context 'as normal user' do
+      it 'renders the current users "edit" template' do
+        get :edit, id: 'nonexistent_id'
+        expect(response).to render_template 'edit'
+        expect(assigns(:user)).to eql @user
+      end
+    end
+  end
+
 end
