a33e74a8037a710794224ced4203e4aab788f10f
Carsten Zimmermann <carp@hacksocke.de>
2013-08-22 14:04:14 +0200

    Use APP_CONFIG in UserMailer and make its specs pass again
    Fix #13

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,7 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: APP_CONFIG[:mail_from]
+
+  default_url_options[:host] = APP_CONFIG[:host]
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -7,8 +9,10 @@ class UserMailer < ActionMailer::Base
   #   en.user_mailer.password_reset.subject
   #
   def password_reset(user)
-    @user = user
-
-    mail :to => user.email, :subject => "Password Reset"
+    @url = edit_password_reset_url(
+      user.password_reset_token,
+      protocol: APP_CONFIG[:url_scheme]
+    )
+    mail to: user.email, subject: "[event_girl] Password reset"
   end
 end
