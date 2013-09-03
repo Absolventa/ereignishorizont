0d316fff72ffa951c9d7c65587a9bc7b696b1ef1
Susanne Dewein <susanne.dewein@gmail.com>
2013-09-03 12:25:19 +0200

    Refactor Matcher class
    Christoph helped us rewrite it so that we don't need the scopes from Incoming Events.

--- app/models/matcher.rb
+++ app/models/matcher.rb
@@ -1,15 +1,29 @@
 class Matcher
   def run
-    ExpectedEvent.active.today.backward.each do |expected_event|
-      incoming_events = IncomingEvent.where(tracked_at: nil).where
-        (title: expected_event.title).where
-        ("created_at > ? AND created_at <= ?", Time.zone.now.beginning_of_day, expected_event.deadline)
-
-        incoming_events.each do |incoming_event|
-          incoming_event.track!
-        end
-        expected_event.alarm! if incoming_events.empty?
+    ExpectedEvent.active.today.backward.each |expected_event|
+      incoming_events = (
+        scope = expected_event.incoming_events
+        scope = scope.where(title: expected_event.title)
+        scope = scope.select_if { |event| event.created_at < expected_event.deadline && event.tracked_at.blank? }
+        scope
+      )
+      incoming_events.each |incoming_event|
+        incoming_event.track!
       end
+      expected_event.alarm! if incoming_events.empty?
     end
+
+
+    # ExpectedEvent.active.today.backward.each do |expected_event|
+    #   incoming_events = IncomingEvent.where(tracked_at: nil).where
+    #     (title: expected_event.title).where
+    #     ("created_at > ? AND created_at <= ?", Time.zone.now.beginning_of_day, expected_event.deadline)
+
+    #     incoming_events.each do |incoming_event|
+    #       incoming_event.track!
+    #     end
+    #     expected_event.alarm! if incoming_events.empty?
+    #   end
+    # end
   end
 end
