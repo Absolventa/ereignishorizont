7244a9dcdf59ffd562a86a6396234f6a67bef0a0
Carsten Zimmermann <carp@hacksocke.de>
2013-09-07 12:44:25 +0200

    Add specs for password_resets#create

--- spec/controllers/password_resets_controller_spec.rb
+++ spec/controllers/password_resets_controller_spec.rb
@@ -10,4 +10,22 @@ describe PasswordResetsController do
       expect(response).to render_template 'new'
     end
   end
+
+  describe 'POST create' do
+    it 'finds user by email and sends reset instructions' do
+      user = FactoryGirl.create(:user)
+      expect do
+        post :create, email: user.email
+      end.to change{ ActionMailer::Base.deliveries.size }.by(1)
+      expect(flash[:notice]).not_to be_blank
+      expect(response).to redirect_to root_path
+    end
+
+    it 'does not give away existence of email' do
+      expect do
+        post :create, email: "nonexistent@example.com"
+      end.not_to change{ ActionMailer::Base.deliveries.size }
+      expect(flash[:notice]).to eql "Password reset instructions sent."
+    end
+  end
 end
