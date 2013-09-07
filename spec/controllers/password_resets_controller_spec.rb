fca731e29a513181faf2c112210b9976144a715f
Carsten Zimmermann <carp@hacksocke.de>
2013-09-07 12:34:17 +0200

    Obey the controller spec format
    Ie. MY format! :)

--- spec/controllers/password_resets_controller_spec.rb
+++ spec/controllers/password_resets_controller_spec.rb
@@ -1,12 +1,13 @@
 require 'spec_helper'
 
 describe PasswordResetsController do
+  render_views
 
-  describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
-      response.should be_success
+  describe 'GET new' do
+    it "renders the 'new' template" do
+      get :new
+      expect(response).to be_success
+      expect(response).to render_template 'new'
     end
   end
-
 end
