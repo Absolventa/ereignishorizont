31cfa886151fb5a99b5c701884e38e2338d8b804
Carsten Zimmermann <carp@hacksocke.de>
2013-09-07 13:22:38 +0200

    Add specs for password_resets#update

--- spec/controllers/password_resets_controller_spec.rb
+++ spec/controllers/password_resets_controller_spec.rb
@@ -44,4 +44,53 @@ describe PasswordResetsController do
       expect(assigns(:user)).to eql user
     end
   end
+
+  describe 'PATCH update' do
+    let(:passwords) do
+      { password: 'helloworld', password_confirmation: 'helloworld' }
+    end
+
+    it 'returns a hard error when email reset token is unknown' do
+      expect do
+        patch :update, id: "qwerty"
+      end.to raise_exception ActiveRecord::RecordNotFound
+    end
+
+    it 'fails if password_reset_token is older than two hours' do
+      user = FactoryGirl.create(:user,
+                                password_reset_token: 'qwerty',
+                                password_reset_sent_at: 121.minutes.ago
+                               )
+      expect do
+        patch :update, id: user.password_reset_token, user: passwords
+      end.not_to change{ user.reload.password_digest }
+      expect(response).to redirect_to new_password_reset_path
+      expect(flash[:alert]).to match 'expired'
+    end
+
+    context 'with a recently sent token' do
+      let(:user) do
+        user = FactoryGirl.build(:user)
+        user.send_password_reset
+        user
+      end
+
+      it 'updates the password' do
+        expect do
+          patch :update, id: user.password_reset_token, user: passwords
+        end.to change{ user.reload.password_digest }
+        expect(response).to redirect_to root_path
+        expect(flash[:notice]).not_to be_blank
+      end
+
+      it 'fails to update user and renders edit' do
+        User.any_instance.stub(:valid?).and_return(false)
+        expect do
+          patch :update, id: user.password_reset_token, user: passwords
+        end.not_to change{ user.reload.password_digest }
+        expect(response).to render_template 'edit'
+      end
+    end
+  end
+
 end
