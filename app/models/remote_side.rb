6fb8b3473402ab6d22d673926d9af9c88ae0b05c
Tam Eastley <tam.eastley@gmail.com>
2013-09-10 14:48:28 +0200

    Create generate api_token method

--- app/models/remote_side.rb
+++ app/models/remote_side.rb
@@ -1,6 +1,15 @@
 class RemoteSide < ActiveRecord::Base
+
   validates :name, presence: true, format: { with: /\A[a-z0-9\s]+\Z/i }
   validates_uniqueness_of :name
-  validates_uniqueness_of :api_token
-end
+  before_create :generate_api_token
 
+  private
+
+  def generate_api_token
+    begin
+      self.api_token = SecureRandom.hex
+    end while self.class.exists?(api_token: api_token)
+  end
+
+end
