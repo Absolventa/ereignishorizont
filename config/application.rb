29cdd36c5d87449b1733fcbad356576970f53af0
Carsten Zimmermann <carp@hacksocke.de>
2013-09-18 17:06:48 +0200

    Add middleware provided by actionpack-xml_parser

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module EventGirl
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     config.time_zone = 'Berlin'
 
+    config.middleware.insert_after ActionDispatch::ParamsParser, ActionDispatch::XmlParamsParser
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
