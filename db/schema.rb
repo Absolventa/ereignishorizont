61a5c04db64830079629def8f251346da88c1fa1
FrauBienenstich <susanne.dewein@gmail.com>
2013-09-24 12:28:55 +0200

    Add comments in schema rb
    These comments pop up again and again, even if I discard them.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130923170930) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "alarms", force: true do |t|
     t.string   "title"
     t.string   "action"
