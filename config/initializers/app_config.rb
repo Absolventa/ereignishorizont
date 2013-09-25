849e6ff4c2555097405954b77b332ece08fd594e
Tam Eastley <tam.eastley@gmail.com>
2013-09-25 16:15:58 +0200

    Add config files for heroku mails

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -7,3 +7,18 @@ rescue Errno::ENOENT => e
   exit 1
 end
 APP_CONFIG = (YAML.load(erb_config)[Rails.env] rescue {}).symbolize_keys
+
+if Rails.env.production? or Rails.env.staging?
+  ActionMailer::Base.smtp_settings = {
+    address:        'smtp.sendgrid.net',
+    port:           '587',
+    authentication: :plain,
+    user_name:      ENV['SENDGRID_USERNAME'],
+    password:       ENV['SENDGRID_PASSWORD'],
+    domain:         'heroku.com'
+  }
+  ActionMailer::Base.delivery_method = :smtp
+elsif Rails.env.development?
+  ActionMailer::Base.delivery_method = :sendmail
+  Mail.register_interceptor(MailInterceptor)
+end
