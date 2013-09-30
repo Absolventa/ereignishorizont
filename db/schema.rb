73271f4615f55c95c977f6638cce8dcfd4c503d9
Susanne Dewein <susanne.dewein@gmail.com>
2013-09-30 16:47:40 +0200

    Update the schema.rb
    Apparently this is fine.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130930091129) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "alarm_notifications", force: true do |t|
     t.integer  "expected_event_id"
     t.datetime "created_at"
