e5ddd1aa74d144d68e877990556736516c556025
Carsten Zimmermann <carp@hacksocke.de>
2013-09-30 12:30:27 +0200

    Add AlarmNotification.today scope

--- app/models/alarm_notification.rb
+++ app/models/alarm_notification.rb
@@ -4,11 +4,13 @@ class AlarmNotification < ActiveRecord::Base
   validates :expected_event, presence: true
   validate  :expected_event_must_be_unique_for_today
 
+  scope :today, -> { where('created_at > ?', Time.zone.now.beginning_of_day) }
+
   private
 
   def expected_event_must_be_unique_for_today
     unless AlarmNotification.where(expected_event_id: self.expected_event_id).
-      where('created_at > ?', Time.zone.now.beginning_of_day).empty?
+      today.empty?
       errors.add(:expected_event_id, 'has already been registered for today' )
     end
   end
