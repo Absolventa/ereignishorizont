0aab0bd806fcbd0eb1652c6dcfdf053ebf0d28cf
Carsten Zimmermann <carp@hacksocke.de>
2013-10-07 11:54:00 +0200

    Remove commented code

--- app/models/matcher.rb
+++ app/models/matcher.rb
@@ -1,20 +1,3 @@
-# class Matcher
-#   def run
-#     ExpectedEvent.active.today.backward.each do |expected_event|
-#       incoming_events = IncomingEvent.not_tracked.
-#       where(title: expected_event.title).
-#       where("created_at > ? AND created_at <= ?", Time.zone.now.beginning_of_day, expected_event.deadline)
-
-#         incoming_events.each do |incoming_event|
-#           incoming_event.track!
-#         end
-#         expected_event.alarm! if incoming_events.empty?
-#       end
-#     end
-#   end
-
-# end
-
 class Matcher
   def run
     expected_events.each do |expected_event|
