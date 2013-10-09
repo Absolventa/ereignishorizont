25f6b1d67e1a0344ae078e22919f5705c28408cf
Carsten Zimmermann <carp@hacksocke.de>
2013-10-09 16:13:02 +0200

    Test parsing of json input
    Also had a brief test with line breaks but could
    not reproduce the error reported in #42

---
+++ spec/requests/expected_events_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe "Expected events" do
+  context 'as an API consumer' do
+    before do
+      @api_token = FactoryGirl.create(:remote_side).api_token
+    end
+
+    context 'using the JSON format' do
+      let(:authentication) { { api_token: @api_token } }
+      let(:accept_headers) { { 'Content-Type' => 'application/json' } }
+
+      it 'creates an incoming event' do
+        json = authentication.merge(incoming_event: { title: 'Foo' }).to_json
+        post '/incoming_events.json', json, accept_headers
+        expect(response.code).to eql "201"
+      end
+    end
+  end
+end
