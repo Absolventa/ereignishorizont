046ea0635bc24db2e9734906aee6bc85c962324b
Carsten Zimmermann <carp@hacksocke.de>
2013-10-11 11:57:09 +0200

    Pass expected event to mailer

--- app/mailers/alarm_mailer.rb
+++ app/mailers/alarm_mailer.rb
@@ -1,11 +1,11 @@
 class AlarmMailer < ActionMailer::Base
   default from: APP_CONFIG[:mail_from]
 
-  def event_expectation_matched(alarm)
+  def event_expectation_matched(alarm, expected_event)
     @alarm = alarm
-    @expected_event = alarm.expected_event.title
+    @expected_event = expected_event.title
     @message = alarm.message
-    @matched = alarm.expected_event.event_matching_direction_for_email
+    @matched = expected_event.event_matching_direction_for_email
     mail(:to => alarm.recipient_email, :subject => alarm.title)
     #mail call should always be at the end
   end
