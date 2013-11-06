8c1bfcba54e27a428f8ef8896e4d0a107cf0e09a
Carsten Zimmermann <carp@hacksocke.de>
2013-11-06 17:13:09 +0100

    Include origin site in mail notification
    Fixes #46

--- app/mailers/alarm_mailer.rb
+++ app/mailers/alarm_mailer.rb
@@ -3,10 +3,12 @@ class AlarmMailer < ActionMailer::Base
 
   def event_expectation_matched(alarm, expected_event)
     @alarm = alarm
-    @expected_event = expected_event.title
+    @expected_event = expected_event
+    @remote_side = expected_event.remote_side.try(:name)
     @message = alarm.message
     @matched = expected_event.event_matching_direction_for_email
-    subject = "#{@alarm.title}: #{@expected_event}"
+    subject_suffix = [@remote_side, @alarm.title].compact.join(' - ')
+    subject = "[event_girl] #{subject_suffix}"
     mail(to: alarm.recipient_email, subject: subject)
   end
 end
