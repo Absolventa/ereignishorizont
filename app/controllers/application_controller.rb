577a8c8421b390b4beac9baf7d4bc1a403ae3d9a
Carsten Zimmermann <carp@hacksocke.de>
2014-02-05 16:15:34 +0100

    Set Time.zone from user for this request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
   before_action :authorize
+  before_action :set_time_zone
 
   private
   def current_user
@@ -18,4 +19,7 @@ class ApplicationController < ActionController::Base
     redirect_to login_path, alert: "Not authorized" unless logged_in?
   end
 
+  def set_time_zone
+    Time.zone = current_user.time_zone if current_user
+  end
 end
