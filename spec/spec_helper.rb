f54af5cfd37571ddf5c7f04100f464f9fead1ad6
Raimund Schramm <raimund.schramm@gmx.net>
2014-03-07 14:07:21 +0100

    adds authentication feature helper to support
    configures rspec to use it

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,6 +39,7 @@ RSpec.configure do |config|
   end
 
   config.include ExpectedEventSpecHelper
+  config.include AuthenticationHelper
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
