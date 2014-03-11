cc66642f7960517386904e94aef6210436c34ad0
Raimund Schramm <raimund.schramm@gmx.net>
2014-03-11 13:06:10 +0100

    adds changes as requested by maintainer

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -9,13 +9,9 @@ describe SessionsController do
 		end
 
   	describe "GET 'new'" do
-      it "is a redirect" do
-        get 'new'
-        expect(response).to be_redirect
-      end
-
     	it "redirects to root URL" do
       	get 'new'
+        expect(response).to be_redirect
         expect(response).to redirect_to root_path
     	end
   	end
@@ -37,10 +33,6 @@ describe SessionsController do
       it "is successful" do
         get 'new'
         expect(response).to be_success
-      end
-
-      it "renders the new template" do
-        get 'new'
         expect(response).to render_template 'new'
       end
     end
@@ -62,20 +54,19 @@ describe SessionsController do
         end
       end
 
-      it "redirects to root URL" do
+      it "redirects to root URL and displays a success message" do
         post 'create', email: user.email, password: user.password
         expect(response).to redirect_to incoming_events_path
+        expect(flash[:notice]).to eq 'Logged in!'
       end
     end
 
     context "not authenticated" do
-      it "redirects to root URL" do
+      it "redirects to root URL and displays an error message" do
         post 'create', email: 'wrong', password: 'login'
+        expect(cookies[:auth_token]).to be_nil
+        expect(cookies.permanent[:auth_token]).to be_nil
         expect(response).to redirect_to root_path
-      end
-
-      it "sets a flash message" do
-        post 'create', email: 'wrong', password: 'login'
         expect(flash[:error]).to eq 'Email or password is invalid.'
       end
     end
@@ -84,18 +75,10 @@ describe SessionsController do
   describe "DELETE 'destroy'" do
     before { cookies[:auth_token] = user.auth_token }
 
-    it "deletes the auth token from the cookies hash" do
+    it "deletes the auth token from the cookies hash, redirects to root and displays a success message" do
       delete 'destroy'
       expect(cookies[:auth_token]).to be_nil
-    end
-
-    it "redirects to root URL" do
-      delete 'destroy'
       expect(response).to redirect_to root_path
-    end
-
-    it "sets a flash message" do
-      delete 'destroy'
       expect(flash[:notice]).to eq 'Logged out!'
     end
   end
