1417a7e2369372a6a64c0da07d41aed47314ed03
Carsten Zimmermann <cz@aegisnet.de>
2015-01-07 16:58:23 +0100

    Update config through rake rails:update

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,8 @@ module EventGirl
     # config.i18n.default_locale = :de
 
     config.middleware.insert_after ActionDispatch::ParamsParser, ActionDispatch::XmlParamsParser
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
