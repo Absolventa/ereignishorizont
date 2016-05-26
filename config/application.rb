efd7de674c62f4adf71cb3202695a70ee43f3858
Carsten Zimmermann <cz@aegisnet.de>
2016-05-26 17:36:02 +0200

    Include middleware differently

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module EventGirl
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.middleware.insert_after ActionDispatch::ParamsParser, ActionDispatch::XmlParamsParser
+    config.middleware.use ActionDispatch::ParamsParser
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
