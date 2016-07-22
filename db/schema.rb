3dacc74adf11849dc8298f139c5176a5046ebb82
Daniel Schoppmann <daniel.schoppmann@gmail.com>
2016-07-22 11:31:42 +0200

    Remove scaffold stubs

--- db/schema.rb
+++ db/schema.rb
@@ -21,25 +21,23 @@ ActiveRecord::Schema.define(version: 20150202102148) do
     t.integer  "expected_event_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["alarm_id"], name: "index_alarm_mappings_on_alarm_id", using: :btree
+    t.index ["expected_event_id"], name: "index_alarm_mappings_on_expected_event_id", using: :btree
   end
-
-  add_index "alarm_mappings", ["alarm_id"], name: "index_alarm_mappings_on_alarm_id", using: :btree
-  add_index "alarm_mappings", ["expected_event_id"], name: "index_alarm_mappings_on_expected_event_id", using: :btree
 
   create_table "alarm_notifications", force: :cascade do |t|
     t.integer  "expected_event_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "remote_side_id"
+    t.index ["expected_event_id"], name: "index_alarm_notifications_on_expected_event_id", using: :btree
+    t.index ["remote_side_id"], name: "index_alarm_notifications_on_remote_side_id", using: :btree
   end
 
-  add_index "alarm_notifications", ["expected_event_id"], name: "index_alarm_notifications_on_expected_event_id", using: :btree
-  add_index "alarm_notifications", ["remote_side_id"], name: "index_alarm_notifications_on_remote_side_id", using: :btree
-
   create_table "alarms", force: :cascade do |t|
-    t.string   "title",           limit: 255
-    t.string   "action",          limit: 255
-    t.string   "email_recipient", limit: 255
+    t.string   "title"
+    t.string   "action"
+    t.string   "email_recipient"
     t.text     "message"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -59,7 +57,7 @@ ActiveRecord::Schema.define(version: 20150202102148) do
     t.boolean  "weekday_4"
     t.boolean  "weekday_5"
     t.boolean  "weekday_6"
-    t.string   "matching_direction", limit: 255
+    t.string   "matching_direction"
     t.integer  "final_hour"
     t.date     "started_at"
     t.date     "ended_at"
@@ -67,9 +65,8 @@ ActiveRecord::Schema.define(version: 20150202102148) do
     t.datetime "updated_at"
     t.integer  "remote_side_id"
     t.integer  "day_of_month"
+    t.index ["remote_side_id"], name: "index_expected_events_on_remote_side_id", using: :btree
   end
-
-  add_index "expected_events", ["remote_side_id"], name: "index_expected_events_on_remote_side_id", using: :btree
 
   create_table "incoming_events", force: :cascade do |t|
     t.text     "title"
@@ -78,27 +75,26 @@ ActiveRecord::Schema.define(version: 20150202102148) do
     t.integer  "expected_event_id"
     t.integer  "remote_side_id"
     t.text     "content"
+    t.index ["expected_event_id"], name: "index_incoming_events_on_expected_event_id", using: :btree
   end
 
-  add_index "incoming_events", ["expected_event_id"], name: "index_incoming_events_on_expected_event_id", using: :btree
-
   create_table "remote_sides", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "api_token",  limit: 255
+    t.string   "name"
+    t.string   "api_token"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255
-    t.string   "password_digest",        limit: 255
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "auth_token",             limit: 255
-    t.string   "password_reset_token",   limit: 255
+    t.string   "auth_token"
+    t.string   "password_reset_token"
     t.datetime "password_reset_sent_at"
     t.boolean  "admin"
-    t.string   "time_zone",              limit: 255, default: "UTC"
+    t.string   "time_zone",              default: "UTC"
   end
 
 end
